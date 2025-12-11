b94f332f97443665cc77a3159e847936c36b9bc9
AaronWang7 <aaron.wang@ucas-edu.net>
2025-12-11 11:48:25 -0700

    Update piece setup in ChessGame to include initial position parameters

--- game.py
+++ game.py
@@ -144,15 +144,16 @@ def run_board():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
                 print("Clicked:", x, y)
-
+                chess_game = ChessGame()
                 board.board_x = x
                 board.board_y = y
                 pos = board.board_get()
+                chess_game.setup_pieces()
                 print("Piece position:", pos)
-                if y <= 75:
-                    if x <= 100:
-                        xx = x + 100
-                        yy = y + 75
-                        white_pawns.append(WhitePawn(0, xx, yy))
-
+                if pos == "A1":
+                    time.sleep(1)
+                    white_pawns.append(WhitePawn(0, xx, yy))
+                    print(xx, yy)
+                    for white_p in white_pawns:
+                        white_p.pawn1_set(screen)
         pygame.display.update()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,9 @@
-    def get_pieces_left(self, color):
-        # Get count of remaining pieces for a color
-        pieces = self.white_pieces if color == "White" else self.black_pieces
-        return len(pieces)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                print("Clicked:", x, y)
+                chess_game = ChessGame()
+                board.board_x = x
+                board.board_y = y
+                pos = board.board_get()
+                chess_game.setup_pieces()
+                print("Piece position:", pos)
